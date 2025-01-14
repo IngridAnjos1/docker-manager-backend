0822a26ebdb9a4b298c57b6be3f3dfeb7c705f93
Ingrid Anjos <ianjos@ctiglobal.com>
2025-01-14 18:18:31 -0300

    feat: adicionados métodos de gerenciamento de containers no DockerService

--- src/main/java/com/ingridanjos/docker_manager/services/DockerService.java
+++ src/main/java/com/ingridanjos/docker_manager/services/DockerService.java
@@ -1,8 +1,43 @@
 package com.ingridanjos.docker_manager.services;
 
 
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.model.Container;
+import com.github.dockerjava.api.model.Image;
 import org.jvnet.hk2.annotations.Service;
 
+import java.util.List;
+
 @Service
+
 public class DockerService {
+    private final DockerClient dockerClient;
+
+    public DockerService(DockerClient client){
+        this.dockerClient = client;
+    }
+
+      public List<Container> listContainers(boolean all) {
+        return dockerClient.listContainersCmd().withShowAll(all).exec();
+    }
+
+    public List<Image> listImagens(){
+        return dockerClient.listImagesCmd().exec();
+    }
+
+    public void startContainer(String containerId){
+        dockerClient.startContainerCmd(containerId).exec();
+    }
+
+    public void stopContainer(String containerId){
+        dockerClient.stopContainerCmd(containerId).exec();
+    }
+
+    public void deleteContainer(String containerId){
+        dockerClient.removeConfigCmd(containerId).exec();
+    }
+
+    public void createContainer(String imageName) {
+        dockerClient.createContainerCmd(imageName).exec();
+    }
 }
